22903d8be0b6d8d3678cdba18c9eba2cca877b5d
asus <test@gmail.com>
2023-05-16 23:26:55 +0700

    commit

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -31,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     private int mColorBackground;
     private int mColorRetangle;
-    private int mColorCircle;
+    private int mColorFace;
     private int mColorText;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         //inisiasi color
         mColorBackground = ResourcesCompat.getColor(getResources(), R.color.colorBackground, null);
         mColorRetangle = ResourcesCompat.getColor(getResources(), R.color.colorRetangle, null);
-        mColorCircle = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
+        mColorFace = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
         mColorText = ResourcesCompat.getColor(getResources(), R.color.black, null);
 
         //mewarnai paint
@@ -54,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                drawSomething(view);
+                drawSomething(view, count);
+                count += 1;
             }
         });
 
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //fungsi untuk menggambar
-    public void drawSomething (View view){
+    public void drawSomething(View view, int count){
         int vWidth = view.getWidth();
         int vHeight = view.getHeight();
 
@@ -71,59 +73,41 @@ public class MainActivity extends AppCompatActivity {
 
 
         //klik pertama
-        if (mOffset == OFFSET) {
+        if (this.count == 0) {
             mBitmap = Bitmap.createBitmap(vWidth, vHeight, Bitmap.Config.ARGB_8888);
             mImageView.setImageBitmap(mBitmap);
             mCanvas = new Canvas(mBitmap);
             mCanvas.drawColor(mColorBackground);
 
-            mCanvas.drawText(getString(R.string.keep_tapping),
-                    100, 100, mPaintText);
-            mOffset += OFFSET;
+//            mCanvas.drawText(getString(R.string.keep_tapping),
+//                    100, 100, mPaintText);
+//            mOffset += OFFSET;
         }
         //klik kedua
-        else{
-            //batasan jumlah rectangle
-            if (mOffset < halfWidth && mOffset < halfHeight) {
-                //menggambar rectangle berkali kali tapi ukurannya berbeda beda
-                mPaint.setColor(mColorRetangle - MULTIPLIER*mOffset);
-                mRect.set(mOffset, mOffset, vWidth-mOffset, vHeight-mOffset);
-                mCanvas.drawRect(mRect, mPaint);
-                //ofset ditambah tambah terus agar ukuran makin kecil
-                mOffset += OFFSET;
-            }
+        else if (this.count == 2) {
+            mPaint.setColor(mColorFace);
+            mCanvas.drawCircle(halfWidth, halfHeight-100, halfHeight/3, mPaint);
+            int x = halfWidth - mBounds.centerX();
+            int y = halfHeight - mBounds.centerY();
 
-            else{
-                //jika jumlah rectangle lebih maka akan digambar lingkaran
-                mPaint.setColor(mColorCircle - MULTIPLIER*mOffset);
-                //menggambar lingkaran dan set teks
-                mCanvas.drawCircle( halfWidth, halfHeight, halfHeight/3, mPaint);
-                String text = getString(R.string.done);
-                mPaintText.getTextBounds(text, 0, text.length(), mBounds);
-                int x = halfWidth - mBounds.centerX();
-                int y = halfHeight - mBounds.centerY();
-                mCanvas.drawText(text, x, y, mPaintText);
-                mOffset += OFFSET;
+        }
 
+        else if (count == 3) {
+            mPaint.setColor(mColorFace - MULTIPLIER *mOffset);
+            Point a = new Point(halfWidth-410, halfHeight-310);
+            Point b = new Point(halfWidth-150, halfHeight-398);
+            Point c = new Point( halfWidth - 330, halfHeight+50);
 
-                //menambahkan segitiga
-                mPaint.setColor(mColorBackground - MULTIPLIER*mOffset);
-                Point a = new Point(halfWidth-50, halfHeight-50);
-                Point b = new Point(halfWidth+50, halfHeight-50);
-                Point c = new Point(halfWidth, halfHeight+250);
+            Path path = new Path();
+            path.setFillType(Path.FillType.EVEN_ODD);
+            path.lineTo(a.x, a.y);
+            path.lineTo(b.x, b.y);
+            path.lineTo(c.x, c.y);
+            path.lineTo(a.x, a.y);
+            path.close();
 
-                Path path= new Path();
-                path.setFillType(Path.FillType.EVEN_ODD);
-                path.lineTo(a.x, a.y);
-                path.lineTo(b.x, b.y);
-                path.lineTo(c.x, c.y);
-                path.lineTo(a.x, a.y);
-                path.close();
+            mCanvas.drawPath(path, mPaint);
 
-                mCanvas.drawPath(path, mPaint);
-                mOffset += OFFSET;
-
-            }
 
         }
 
